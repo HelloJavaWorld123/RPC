9cdf44ef55a0412bfb34e8a7d2ed56640eb75927
xiaokuan.ren <renxiaokuan9428@duia.com>
2019-10-31 13:53:11 +0800

    增加项目基本结构

---
+++ rpc-consumer/src/main/java/com/rwx/rpc/RpcConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.rwx.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author admin
+ */
+@SpringBootApplication
+public class RpcConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpcConsumerApplication.class, args);
+	}
+
+}
---
+++ rpc-consumer/src/test/java/com/rwx/rpc/RpcConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rwx.rpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
